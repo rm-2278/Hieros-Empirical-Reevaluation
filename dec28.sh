e2666bcb36596b4e9cfd40ca66849d3ea79e6d32
rm-2278 <rm2278@cam.ac.uk>
2025-12-30 14:24:52 +0000

    Added entropy experiment

--- dec28.sh
+++ dec28.sh
@@ -2,7 +2,7 @@
 
 # Baseline reproduction for atari100k (This will take a long time like 4-5 days)
 SWEEP_OUTPUT=$(wandb sweep experiments/pinpad-easy.yml 2>&1)
-SWEEP_ID=$(echo "$SWEEP_OUTPUT" | grep -o 'wandb agent [^ ]*' | awk '{print $3}')
+SWEEP_ID=$(echo "$SWEEP_OUTPUT" | grep -o 'wandb agent [^ ]*' | awk '{print $NF}')
 # Print ID to debug
 echo "Detected Sweep ID: $SWEEP_ID"
-wandb agent $SWEEP_IDd
+wandb agent $SWEEP_ID
